2d0a488962126fb8c56c93e0ed99781ba0679b04
Josh <88543992+VGCCodes@users.noreply.github.com>
2021-10-21 20:33:16 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// ** IMPORTS ** // 
+
+import { Client, Constants, Intents } from "discord.js";
+import db from "quick.db";
+import commandLoader from "./Util/commandLoader.js";
+import eventLoader from "./Util/eventLoader.js";
+const config = await import("./config.js");
+
+// ** Main Code ** //
+
+const client = new Client({intents: Object.values(Intents.FLAGS), partials: Object.values(Constants.PartialTypes)}); // Initialises a new discord.js client
+client.config = config.default;
+client.db = db;
+
+commandLoader(client, "commands") // Loads commands from the specified folder
+eventLoader(client, "events") // Loads events from the specified folder
+
+
+client.login(client.config.token) // Logs in to your bot using the specified token
